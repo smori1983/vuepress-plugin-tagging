104247e34693cb23f6c9702a336b80aa2c5f2d14
smori <shinichiro.mori.1983@gmail.com>
2021-12-26 01:47:16 +0900

    Rename TagList.vue to TagListType1.vue

--- src/enhanceAppFile.js
+++ src/enhanceAppFile.js
@@ -1,7 +1,7 @@
-import TagList from './TagList';
+import TagListType1 from './TagListType1';
 import TagListType2 from './TagListType2';
 
 export default ({ Vue }) => {
-  Vue.component('TaggingTagList', TagList);
+  Vue.component('TaggingTagListType1', TagListType1);
   Vue.component('TaggingTagListType2', TagListType2);
 };
