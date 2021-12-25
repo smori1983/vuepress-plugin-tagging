e1d07d2ecb404f2d14ac9d17e2791dd6327c4695
smori <shinichiro.mori.1983@gmail.com>
2021-12-26 01:01:11 +0900

    Implement clientDynamicModules to prepare tag list

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 const { path } = require('@vuepress/shared-utils');
 
-module.exports = {
+module.exports = (options, ctx) => ({
   enhanceAppFiles: [
     path.resolve(__dirname, 'enhanceAppFile.js'),
   ],
-};
+  clientDynamicModules() {
+    const tagsMemo = {};
+    ctx.pages.forEach((page) => {
+      const tags = page.frontmatter.tags || [];
+      tags.forEach((tag) => {
+        tagsMemo[tag] = tagsMemo[tag] || [];
+        tagsMemo[tag].push({
+          key: page.key,
+          title: page.title,
+          regularPath: page.regularPath,
+        });
+      });
+    });
+
+    for (const tag in tagsMemo) {
+      tagsMemo[tag].sort((a, b) => {
+        return a.title < b.title ? -1 : 1;
+      });
+    }
+
+    const tagList = [];
+    for (const tag in tagsMemo) {
+      tagList.push({
+        name: tag,
+        pages: tagsMemo[tag],
+      });
+    }
+
+    return [
+      {
+        name: 'vuepress-plugin-tagging/tag-list.js',
+        content: `export default ${JSON.stringify(tagList, null, 2)}`,
+      },
+    ];
+  },
+});
