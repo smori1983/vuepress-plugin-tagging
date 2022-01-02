24c082c14609c65887fa2723a6dbbf07f1cb813c
smori <shinichiro.mori.1983@gmail.com>
2022-01-02 23:11:00 +0900

    Maintain example_i18n files

--- example_i18n/.vuepress/config.js
+++ example_i18n/.vuepress/config.js
@@ -22,8 +22,8 @@ module.exports = {
           { text: 'Tag4', link: '/tag4.html' },
         ],
         sidebar: [
-          { collapsable: false, title: 'cat1', children: ['/cat1/page1', '/cat1/page2', '/cat1/page3'] },
-          { collapsable: false, title: 'cat2', children: ['/cat2/page4', '/cat2/page5', '/cat2/page6'] },
+          { collapsable: false, title: 'cat1', children: ['/cat1/page1', '/cat1/page2'] },
+          { collapsable: false, title: 'cat2', children: ['/cat2/page4', '/cat2/page5'] },
         ],
       },
       '/ja/': {
@@ -36,8 +36,8 @@ module.exports = {
           { text: 'タグ一覧4', link: '/ja/tag4.html' },
         ],
         sidebar: [
-          { collapsable: false, title: 'カテゴリ1', children: ['/ja/cat1/page1', '/ja/cat1/page2', '/ja/cat1/page3'] },
-          { collapsable: false, title: 'カテゴリ2', children: ['/ja/cat2/page4', '/ja/cat2/page5', '/ja/cat2/page6'] },
+          { collapsable: false, title: 'カテゴリ1', children: ['/ja/cat1/page1', '/ja/cat1/page2'] },
+          { collapsable: false, title: 'カテゴリ2', children: ['/ja/cat2/page4', '/ja/cat2/page5'] },
         ],
       },
     },
