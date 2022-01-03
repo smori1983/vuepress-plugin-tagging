5866b6f11be0adb18282ec7c675b2dc7f80ed357
smori <shinichiro.mori.1983@gmail.com>
2022-01-03 21:58:46 +0900

    Rename debug component

--- src/enhanceAppFile.js
+++ src/enhanceAppFile.js
@@ -2,12 +2,12 @@ import TagListAllType1 from './TagListAllType1';
 import TagListAllType2 from './TagListAllType2';
 import TagListI18nType1 from './TagListI18nType1';
 import TagListI18nType2 from './TagListI18nType2';
-import TagListDebug from './TagListDebug';
+import Debug from './Debug';
 
 export default ({ Vue }) => {
   Vue.component('PluginTaggingTagListAllType1', TagListAllType1);
   Vue.component('PluginTaggingTagListAllType2', TagListAllType2);
   Vue.component('PluginTaggingTagListI18nType1', TagListI18nType1);
   Vue.component('PluginTaggingTagListI18nType2', TagListI18nType2);
-  Vue.component('PluginTaggingTagListDebug', TagListDebug);
+  Vue.component('PluginTaggingTagListDebug', Debug);
 };
